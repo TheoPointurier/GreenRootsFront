6c0ea5bc7771ec555f8840138d55543c38b5e49c
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-28 17:07:37 +0100

    initialisation du projet, structure generale

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src//*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        greenroots_green: '#6A9C89',
+        greenroots_orange: '#CD5C08',
+        greenroots_grey: '#FFF9F3',
+      }
+    },
+  },
+  plugins: [],
+};
+
