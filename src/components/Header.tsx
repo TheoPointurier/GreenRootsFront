11c9de599bb7f563e89f46c2d689a9e9a5602042
Yohan SEGUIN <seguinyohan@hotmail.com>
2024-10-29 11:11:27 +0100

    Add first test logos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import logo from '../assets/Logos/Logo_principal.webp';
+
 function Header () {
   return (
-    <header className="header">
-      <div className="div-title">
+    <header className="flex items-center justify-between gap-2 p-4 shadow">
+      <img src={logo} alt="logo" className="w-10 h-10 md:w-14 md:h-14" />
+      <div>
         <h1>APERCU HEADER</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur esse molestias rem! Deserunt placeat aspernatur sapiente maiores voluptatibus natus consectetur recusandae voluptas culpa corrupti accusantium, necessitatibus non quisquam perferendis at!</p>
       </div>
