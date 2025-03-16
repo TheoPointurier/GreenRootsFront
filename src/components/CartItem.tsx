60aaf6071f6b468138f6966e6d330ef844c64c2e
theonag <theopointurier@gmail.com>
2025-03-16 07:46:22 +0100

    fix img in cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -13,7 +13,7 @@ function CartItem({ item, quantity }: CartItemProps) {
   {/* Image */}
   <div className="flex justify-center w-full md:w-1/3">
     <img
-      src={`${import.meta.env.VITE_IMG_URL}/${item.image}`}
+      src={`${item.image}`}
       alt={item.name}
       className="w-full h-40 md:h-full rounded-t-[20px] md:rounded-tl-[20px] md:rounded-bl-[20px] md:rounded-t-none object-cover"
       loading="lazy"
